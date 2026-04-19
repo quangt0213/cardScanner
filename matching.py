6cd5df3fe63143e076c5c30517877f25a492038a
quangt0213 <143447958+quangt0213@users.noreply.github.com>
2026-04-19 01:44:06 -0700

    Refactor match_card function to use get_cards_for_matching

--- matching.py
+++ matching.py
@@ -1,44 +1,30 @@
-import sqlite3
+from database import get_cards_for_matching
 from rapidfuzz import fuzz
 
 #matching
-def match_card(ocr_results, db_path = "data/carddata.db"):
-	ocr_text = " ".join([x["cleaned"] for x in ocr_results 
-	if x["confidence"] > 0.30]).upper()
+def match_card(ocr_results, game="onepiece"):
+	raw_text = " ".join([x["raw"] for x in ocr_results])
+	normalized_text = " ".join([x["cleaned"] for x in ocr_results if x["confidence"] > 0.30])
 	
-	print("OCR Text: ", ocr_text)
-	
-	if not ocr_text:
-		return {
-			"best_match": None,
-			"score": 0,
-			"ocr_text": ""
-		}
-
-	#DB connection
-	conn = sqlite3.connect(db_path)
-	cursor = conn.cursor()
-
-	rows = cursor.execute(
-			"SELECT name FROM cards WHERE game= ?",
-			("onepiece",)).fetchall()
-
-	conn.close()
-	
+	cards = get_cards_for_matching(game)	
+ 
 	best_match = None
 	best_score = 0
 	
-	for row in rows:
-		name = row[0].upper()
-		score = fuzz.partial_ratio(ocr_text, name)
+	for card_id, name, normalized_name, set_code in cards:
+		score = fuzz.partial_ratio(normalized_text, normalized_name)
 		
 		if score > best_score:
 			best_score = score
-			best_match = row[0]
+			best_match = {
+				"card_id": card_id,
+				"name": name,
+				"set_code": set_code,
+				"score": float(score)
+            }
 			
 		return {
-			"best match": best_match,
-			"score": float(best_score),
-			"ocr_text": ocr_text
+			"raw_ocr_text": raw_text,
+			"normalized_ocr_text": normalized_text,
+			"best_match": best_match
 		}
-
